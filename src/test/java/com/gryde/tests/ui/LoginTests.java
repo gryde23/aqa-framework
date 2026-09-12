a41fb2fb95b0038bf58faa3bc77f3e6f8220f679
gryde23 <126186165+gryde23@users.noreply.github.com>
2026-09-12 18:56:32 +0300

    test:add tests and testdata for loginPage

--- src/test/java/com/gryde/tests/ui/LoginTests.java
+++ src/test/java/com/gryde/tests/ui/LoginTests.java
@@ -1,12 +1,15 @@
 package com.gryde.tests.ui;
 
-import com.codeborne.selenide.Selenide;
 import com.gryde.pages.LoginPage;
 import com.gryde.tests.base.BaseUITest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-import static com.codeborne.selenide.Selenide.webdriver;
-import static com.codeborne.selenide.WebDriverConditions.urlContaining;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTestId;
+import static com.codeborne.selenide.Selenide.$;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LoginTests extends BaseUITest {
 
@@ -18,6 +21,41 @@ public class LoginTests extends BaseUITest {
                 .enterPassword("secret_sauce")
                 .successLogin();
 
-        webdriver().shouldHave(urlContaining("/inventory.html"));
+        $(byTestId("title")).shouldHave(text("Products"));
     }
+
+    @Test
+    void loginLockedOutUserShowsError() {
+        String error = new LoginPage()
+                .open()
+                .enterUsername("locked_out_user")
+                .enterPassword("secret_sauce")
+                .failedLogin()
+                .getError();
+
+        assertThat(error).isEqualTo("Epic sadface: Sorry, this user has been locked out.");
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(
+            resources = "/testdata/invalid_login_data.csv",
+            numLinesToSkip = 1,
+            delimiter = ';',
+            emptyValue = "")
+    void shouldShowErrorOnInvalidLogin(
+            String username,
+            String password,
+            String expectedError
+    ) {
+        String error = new LoginPage()
+                .open()
+                .enterUsername(username)
+                .enterPassword(password)
+                .failedLogin()
+                .getError();
+
+        assertThat(error).isEqualTo(expectedError);
+    }
+
+
 }
