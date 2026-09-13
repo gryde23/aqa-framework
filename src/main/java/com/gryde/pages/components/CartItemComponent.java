ac18b3335b0d592f0a799a80f15931bebcd7604c
gryde23 <126186165+gryde23@users.noreply.github.com>
2026-09-13 17:21:53 +0300

    test: add component for item in cart

---
+++ src/main/java/com/gryde/pages/components/CartItemComponent.java
@@ -0,0 +1,25 @@
+package com.gryde.pages.components;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byTestId;
+
+public class CartItemComponent extends BaseComponent{
+
+    public CartItemComponent(SelenideElement root) {
+        super(root);
+    }
+
+    public String getItemName() {
+        return root().$(byTestId("inventory-item-name")).shouldBe(Condition.visible).getText();
+    }
+
+    public String getPrice() {
+        return root().$(byTestId("inventory-item-price")).shouldBe(Condition.visible).getText();
+    }
+
+    public void removeItem() {
+        root().$(".cart-button").click();
+    }
+}
